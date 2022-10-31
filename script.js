7d80b14a0713691df06b2daabf5a7a892c359c5d
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2022-10-31 22:22:48 +0200

    1st commit

--- script.js
+++ script.js
@@ -4,13 +4,20 @@ function lock() {
     document.getElementById('button3').setAttribute("disabled", "");
 }
 
-function answer(max) {
-    let ans = Math.floor(Math.random() * max);
-    if (ans == 1) {
-        document.getElementById('output').innerHTML = '<br> Congrats! You won!';
-    } else if (ans == 0) {
-        document.getElementById('output').innerHTML = '<br> Wrong button! Try again!';
-    }
+function firstButton() {
+    document.getElementById('output').innerHTML = '<br> Wrong button! Try again!';   
+    lock();
+    console.log('The code is working!');
+}
+
+function secondButton() {
+    document.getElementById('output').innerHTML = '<br> Wrong button! Try again!';
+    lock();
+    console.log('The code is working!');   
+}
+
+function thirdButton() {
+    document.getElementById('output').innerHTML = '<br> Congrats! You won!';
     lock();
     console.log('The code is working!');
 }
