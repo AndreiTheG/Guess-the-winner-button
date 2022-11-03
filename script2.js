7c981ab68d6629d0ee80cd93458cf16ea5c1c591
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2022-11-03 12:44:47 +0200

    9th Commit

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ function button() {
     document.getElementById('sentence').innerHTML = 'Choose the winner button!';
     for (let i = 0; i < numButtons; i++) {
         document.getElementById('button').innerHTML += '<button id="button' + (i + 1) +'" type="button"' 
-        + 'class="btn btn-primary" value = "' + i + 1 + '"" onclick="answer(2)">Button ' + (i + 1) + '</button> ';
+        + 'class="btn btn-primary" value = "' + i + 1 + '"" onclick="answer()">Button ' + (i + 1) + '</button> ';
     }
     document.getElementById('submit').disabled = true;
 }
@@ -16,12 +16,23 @@ function disableBtn() {
 }
 
 function answer(max) {
-    let numButton = 2;
-    let ans = Math.floor(Math.random() * max);
+    let numButtons = document.querySelector('input').value;
+    let winnerButton = 2, isWinnerButton = false;
+    for (let i = 0; i < numButtons; i++) {
+        if (winnerButton == i + 1) {
+            document.getElementById('ans').innerHTML = '<br>Congrats! You won!';
+            isWinnerButton = true;
+            break;
+        }
+    }
+    if (isWinnerButton == false) {
+        document.getElementById('ans').innerHTML = '<br>Congrats! You won!';
+    }
+    /*let ans = Math.floor(Math.random() * max);
     if (ans == 1) {
         document.getElementById('ans').innerHTML = '<br>Congrats! You won!';
     } else if (ans == 0) {
         document.getElementById('ans').innerHTML = '<br>Wrong button! Try again!';
-    }
+    }*/
     disableBtn();
 }
