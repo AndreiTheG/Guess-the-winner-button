0bb88741c4b3b9c4e9ffc2d110f5899ae726d43e
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2022-11-03 12:36:51 +0200

    7th Commit

--- script2.js
+++ script2.js
@@ -3,7 +3,7 @@ function button() {
     document.getElementById('sentence').innerHTML = 'Choose the winner button!';
     for (let i = 0; i < numButtons; i++) {
         document.getElementById('button').innerHTML += '<button id="button' + (i + 1) +'" type="button"' 
-        + 'class="btn btn-primary" onclick="answer(2)">Button ' + (i + 1) + '</button> ';
+        + 'class="btn btn-primary" value = ' + i + 1 + 'onclick="answer(2)">Button ' + (i + 1) + '</button> ';
     }
     document.getElementById('submit').disabled = true;
 }
@@ -16,8 +16,7 @@ function disableBtn() {
 }
 
 function answer(max) {
-    let numButtons = document.getElementById('button').value;
-    alert(numButtons);
+    let numButton = 2;
     let ans = Math.floor(Math.random() * max);
     if (ans == 1) {
         document.getElementById('ans').innerHTML = '<br>Congrats! You won!';
