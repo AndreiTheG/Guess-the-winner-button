6836806d691393b6872475d9e3f77fa55334c009
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2022-11-07 12:17:56 +0200

    20th Commit

--- script2.js
+++ script2.js
@@ -1,9 +1,11 @@
 function button() {
     let numButtons = document.querySelector('input').value;
+     //randomise choose the winner button
+    let winnerButton = Math.floor(Math.random() * numButtons + 1);
     document.getElementById('sentence').innerHTML = 'Choose the winner button!';
     for (let i = 0; i < numButtons; i++) {
         document.getElementById('button').innerHTML += '<button id="button' + (i + 1) +'" type="button"' 
-        + 'class="btn btn-primary" value = ""' + i + 1 + '"" onclick="answer(' + (i + 1) + ')" ">Button ' + (i + 1) + '</button> ';
+        + 'class="btn btn-primary" value = ""' + i + 1 + '"" onclick="answer(' + (i + 1) + ',' + winnerButton + ')" ">Button ' + (i + 1) + '</button> ';
     }
     document.getElementById('submit').disabled = true;
 }
@@ -15,10 +17,7 @@ function disableBtn() {
     }
 }
 
-function answer(value) {
-    let numButtons = document.querySelector('input').value;
-    //randomise choose the winner button
-    let winnerButton = Math.floor(Math.random() * numButtons + 1);
+function answer(value, winnerButton) {
     if (value == winnerButton) {
         document.getElementById('ans').innerHTML = '<br>Congrats! You won!';
     } else {
