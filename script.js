3c24fb32dcb8f2d299870f90a8335b3c38484da8
AndreiTheG <58601704+AndreiTheG@users.noreply.github.com>
2022-11-09 20:21:51 +0200

    30th Commit

--- script.js
+++ script.js
@@ -12,7 +12,11 @@ function randomWinnerButton() {
 function answer() {
     let firstButton = 1, secButton = 2, thirdButton = 3, winnerButton = randomWinnerButton();
     console.log(winnerButton);
-    if (winnerButton == firstButton || winnerButton == secButton || winnerButton == thirdButton) {
+    if (winnerButton == firstButton && winnerButton != secButton && winnerButton != thirdButton) {
+        document.getElementById('output').innerHTML = '<br> Congrats! You won!';
+    } else if (winnerButton != firstButton && winnerButton == secButton && winnerButton != thirdButton) {
+        document.getElementById('output').innerHTML = '<br> Congrats! You won!';
+    } else if (winnerButton != firstButton && winnerButton != secButton && winnerButton == thirdButton) {
         document.getElementById('output').innerHTML = '<br> Congrats! You won!';
     } else {
         document.getElementById('output').innerHTML = '<br> Wrong button! Try again!';   
